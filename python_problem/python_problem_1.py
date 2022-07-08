b3c584b7ce80666054f8a21c3a18f6da45f7203c
doongdang <whtjrgus0428@gmail.com>
2022-07-08 16:07:33 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,51 +1,34 @@
-from xml.etree.ElementTree import TreeBuilder
-
-
 curNum = 0
 num = 0
-end = False
 
+def brGame(player):
+    global curNum
+    global num
+    while(1):
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        except:
+            print("정수를 입력하세요")
+        else:
+            if num != 1 and num != 2 and num != 3:
+                print("1, 2, 3 중 하나를 입력하세요")
+            else:
+                break;
+
+    for _ in range(1, num+1):
+        curNum += 1
+        print(f"player {player} : {curNum}")
+        if(curNum == 31):
+            return False
 
 while(1):
-    while(1):
-        try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        except:
-            print("정수를 입력하세요")
-        else:
-            if num != 1 and num != 2 and num != 3:
-                print("1, 2, 3 중 하나를 입력하세요")
-            else:
-                break;
-
-    for i in range(1, num+1):
-        curNum += 1
-        print(f"player A : {curNum}")
-        if(curNum == 31):
-            print("playerB win!")
-            end = True
-            break
-    if(end):
-        break
-
-    while(1):
-        try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        except:
-            print("정수를 입력하세요")
-        else:
-            if num != 1 and num != 2 and num != 3:
-                print("1, 2, 3 중 하나를 입력하세요")
-            else:
-                break;
-
-    for i in range(1, num+1):
-        curNum += 1
-        print(f"player B : {curNum}")
-        if(curNum == 31):
-            print("playerA win!")
-            end = True
-            break
-    if(end):
-        break
+    a = brGame("A")
+    if a == False :
+        print("playerB win!")
+        break;
+    b = brGame("B")
+    if b == False :
+        print("playerA win!")
+        break;
+    
 
