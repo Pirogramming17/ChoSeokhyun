ded809fc047aad49a79decb5e0ee43dc3c982b7f
doongdang <whtjrgus0428@gmail.com>
2022-07-08 15:43:38 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,33 +1,48 @@
+from xml.etree.ElementTree import TreeBuilder
+
+
 curNum = 0
 num = 0
+end = False
 
 while(1):
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-    except:
-        print("정수를 입력하세요")
-    else:
-        if num != 1 and num != 2 and num != 3:
-            print("1, 2, 3 중 하나를 입력하세요")
+    while(1):
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        except:
+            print("정수를 입력하세요")
         else:
-            break;
+            if num != 1 and num != 2 and num != 3:
+                print("1, 2, 3 중 하나를 입력하세요")
+            else:
+                break;
 
-for i in range(1, num+1):
-    curNum += 1
-    print(f"player A : {curNum}")
+    for i in range(1, num+1):
+        curNum += 1
+        print(f"player A : {curNum}")
+        if(curNum == 31):
+            end = True
+            break
+    if(end):
+        break
 
-while(1):
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-    except:
-        print("정수를 입력하세요")
-    else:
-        if num != 1 and num != 2 and num != 3:
-            print("1, 2, 3 중 하나를 입력하세요")
+    while(1):
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        except:
+            print("정수를 입력하세요")
         else:
-            break;
+            if num != 1 and num != 2 and num != 3:
+                print("1, 2, 3 중 하나를 입력하세요")
+            else:
+                break;
 
-for i in range(1, num+1):
-    curNum += 1
-    print(f"player B : {curNum}")
+    for i in range(1, num+1):
+        curNum += 1
+        print(f"player B : {curNum}")
+        if(curNum == 31):
+            end = True
+            break
+    if(end):
+        break
 
