b0d2e3adc18638df2d546b5bc039e1d2bb7cc569
doongdang <whtjrgus0428@gmail.com>
2022-07-14 23:20:02 +0900

    Function 6 && Function 7 && Function 9 && Function 11

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('create', views.create, name="create"),
     path('review/<int:id>', views.detail, name="detail"),
+    path('modify/<int:id>', views.modify, name="modify"),
+    path('delete/<int:id>', views.delete, name="delete"),
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -37,3 +37,27 @@ def detail(request, id):
     }
 
     return render(request, template_name="reviews/detail.html", context=context)
+
+def modify(request, id):
+    if request.method == "POST":
+        title = request.POST["title"]
+        release = request.POST["release"]
+        genre = request.POST["genre"]
+        star = request.POST["star"]
+        time = request.POST["time"]
+        content = request.POST["content"]
+        director = request.POST["director"]
+        actor = request.POST["actor"]
+
+        Review.objects.filter(id=id).update(title=title, release=release, genre=genre, star=star, time=time, content=content, director=director, actor=actor)
+        return redirect(f"/review/{id}")
+    else:
+        review = Review.objects.get(id=id)
+        context = {
+            "review" : review
+        }
+        return render(request, template_name="reviews/modify.html", context=context)
+def delete(request, id):
+    if request.method == "POST":
+        Review.objects.filter(id=id).delete()
+    return redirect("/")
