80b1f5e8f489e993258b6bb8630b33f288ae1271
doongdang <whtjrgus0428@gmail.com>
2022-07-14 21:37:51 +0900

    Funtion 1

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-
+    path('', views.home, name="home")
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Review
 
 # Create your views here.
+def home(request):
+    reviews = Review.objects.all()
+
+    context = {
+        "reviews" : reviews
+    }
+
+    return render(request, template_name="reviews/home.html", context=context)
