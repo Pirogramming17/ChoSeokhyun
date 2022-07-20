612ddc7122967c29fb37fae764593c06769d6418
doongdang <whtjrgus0428@gmail.com>
2022-07-20 10:08:10 +0900

    update migrations

--- SWIDEA_SITE/posts/views.py
+++ SWIDEA_SITE/posts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Idea, DevTool
 
 # Create your views here.
+def ideaHome(request):
+    pass
+
+def ideaCreate(request):
+    pass
+
+def ideaDetail(request):
+    pass
+
+def ideaUpdate(request):
+    pass
+
+def devHome(request):
+    pass
+
+def devCreate(request):
+    pass
+
+def devDetail(request):
+    pass
+
+def devUpdate(request):
+    pass
